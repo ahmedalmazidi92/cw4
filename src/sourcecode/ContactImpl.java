c99086ec69b7920cf9a399e9760931f0f7cd70c5
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-30 13:25:18 +0300

    Created implementation of interface Contact

---
+++ src/sourcecode/ContactImpl.java
@@ -0,0 +1,34 @@
+package sourcecode;
+import interfaces.Contact;
+
+public class ContactImpl implements Contact{
+
+	public ContactImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNotes(String note) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
