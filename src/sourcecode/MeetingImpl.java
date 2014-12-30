905fa49dc8010e4c9df9cf03c5a71225d169f93c
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-30 18:46:27 +0300

    Created implementation of Meeting with implementation of just getID()

---
+++ src/sourcecode/MeetingImpl.java
@@ -0,0 +1,32 @@
+package sourcecode;
+
+import java.util.Calendar;
+import java.util.Set;
+import interfaces.Contact;
+import interfaces.Meeting;
+
+public class MeetingImpl implements Meeting {
+	private int id;
+	private Calendar date;
+	private Set<Contact> contacts;
+	public static int count = 0;
+	
+	public MeetingImpl(Calendar date, Set<Contact> contacts) {
+		count++;
+		this.date = date;
+		this.contacts = contacts;
+		this.id = count;
+	}
+	public int getId() {
+		return id;
+	}
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
