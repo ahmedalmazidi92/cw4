8dc5f5ef2ab0ab54bf40588a87c13ed9782a7c0b
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-30 18:27:06 +0300

    Added tests for getId()

--- src/tests/MeetingTest.java
+++ src/tests/MeetingTest.java
@@ -1,14 +1,29 @@
 package tests;
-
+import java.util.Calendar;
+import java.util.Set;
+import interfaces.Meeting;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class MeetingTest {
-
+	private Meeting test1 = new MeetingImpl();
+	private Meeting test2 = new MeetingImpl();
+	private Meeting test3 = new MeetingImpl();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testID() {
+		int expected = 1;
+		int actual = test1.getId();
+		assertEquals(expected, actual);
+		
+		expected = 2;
+		actual = test2.getId();
+		assertEquals(expected, actual);
+		
+		expected = 3;
+		actual = test3.getId();
+		assertEquals(expected, actual);
 	}
 
 }
