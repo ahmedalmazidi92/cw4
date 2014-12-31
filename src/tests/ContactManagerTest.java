dc5ad5f0d816bc9a6397f9365536350b963ac861
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-31 17:54:18 +0300

    Addded expected fields

--- src/tests/ContactManagerTest.java
+++ src/tests/ContactManagerTest.java
@@ -1,22 +1,50 @@
 package tests;
 import interfaces.Contact;
+import interfaces.ContactManager;
+import interfaces.Meeting;
+
+import org.junit.*;
+import org.junit.Assert.*;
+
+import sourcecode.ContactImpl;
+import sourcecode.FutureMeetingImpl;
+import sourcecode.MeetingImpl;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
 import java.util.Set;
 
 import interfaces.FutureMeeting;
 
 public class ContactManagerTest {
-	private FutureMeeting test1;
+	private ContactManager test1;
+	private Meeting meeting1;
 	private Calendar date1;
 	private Set<Contact> contacts1;
-	private 
+	private Contact simon;
+	private Contact nick;
+	private Contact edgar;
+	private Contact scott;
 
 	public ContactManagerTest() {
+		simon = new ContactImpl("Simon Pegg", "Recovering from Paul");
+		nick = new ContactImpl("Nick Frost", "Also recovering from Paul");
+		edgar = new ContactImpl("Edgar Wright", "Is glad to not have worked on Paul");
+		scott = new ContactImpl("Scott Pilgrim");
 		date1 = new GregorianCalendar(2015, 03, 05);
 		contacts1 = new HashSet<Contact>();
-		test1 = new FutureMeetingImpl() 
+		contacts1.add(simon);
+		contacts1.add(nick);
+		contacts1.add(edgar);
+		contacts1.add(scott);
+		meeting1 = new FutureMeetingImpl(date1, contacts1);
+	}
+	
+	@After
+	public void cleanUp() {
+		MeetingImpl.count = 0;
+		ContactImpl.count = 0;
 	}
 
 }
