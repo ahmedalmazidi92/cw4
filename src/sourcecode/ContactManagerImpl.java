dc38dcb00e42bc19447cfca37782766a366274c9
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-31 18:06:19 +0300

    Created implementation of ContactManager

---
+++ src/sourcecode/ContactManagerImpl.java
@@ -0,0 +1,98 @@
+package sourcecode;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
+import interfaces.Contact;
+import interfaces.ContactManager;
+import interfaces.FutureMeeting;
+import interfaces.Meeting;
+import interfaces.PastMeeting;
+
+public class ContactManagerImpl implements ContactManager {
+
+	public ContactManagerImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PastMeeting getPastMeeting(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FutureMeeting getFutureMeeting(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Meeting getMeeting(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Meeting> getFutureMeetingList(Contact contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Meeting> getFutureMeetingList(Calendar date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PastMeeting> getPastMeetingList(Contact contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNewPastMeeting(Set<Contact> contacts, Calendar date,
+			String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addMeetingNotes(int id, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewContact(String name, String notes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<Contact> getContacts(int... ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
