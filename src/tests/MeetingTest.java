0f8e725541e26be153e346468fd0bc32dc9e676f
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-31 12:58:54 +0300

    Added a 4th test and more member fields

--- src/tests/MeetingTest.java
+++ src/tests/MeetingTest.java
@@ -1,16 +1,29 @@
 package tests;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Set;
+
+import interfaces.Contact;
 import interfaces.Meeting;
+import sourcecode.ContactImpl;
 import sourcecode.MeetingImpl;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class MeetingTest {
-	private Meeting test1 = new MeetingImpl();
-	private Meeting test2 = new MeetingImpl();
-	private Meeting test3 = new MeetingImpl();
+	private Meeting test1;
+	private Meeting test2; 
+	private Meeting test3;
+	private Meeting test4;
+	private Set<Contact> contacts;
+	private Calendar date;
+	
+	@Before
+	public void buildUp() {
+		contacts.add(new ContactImpl("Simon Pegg"));
+		contacts.add(new ContactImpl("Nick Frost"));
+	}
 	
 	@Test
 	public void testID() {
@@ -24,6 +37,10 @@ public class MeetingTest {
 		
 		expected = 3;
 		actual = test3.getId();
+		assertEquals(expected, actual);
+		
+		expected = 4;
+		actual = test4.getId();
 		assertEquals(expected, actual);
 	}
 
