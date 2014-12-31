4a4e0c9ae6cec4168743fafaac72a16c77acb362
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-31 14:27:01 +0300

    Created implementation for FutureMeeting

---
+++ src/sourcecode/FutureMeetingImpl.java
@@ -0,0 +1,15 @@
+package sourcecode;
+
+import interfaces.Contact;
+import interfaces.FutureMeeting;
+
+import java.util.Calendar;
+import java.util.Set;
+
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+
+	public FutureMeetingImpl(Calendar date, Set<Contact> contacts) {
+		super(date, contacts);
+	}
+
+}
