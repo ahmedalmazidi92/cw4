d46901ad40b05249bb41cc12f8ba0e32308c9c48
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-01-01 13:05:07 +0300

    Changed assertTrue to assertFalse

--- src/tests/ContactManagerTest.java
+++ src/tests/ContactManagerTest.java
@@ -71,7 +71,7 @@ public class ContactManagerTest {
 		String name = "Simon";
 		String notes = "Test";
 		test1.addNewContact(name, notes);
-		assertTrue(test1.isEmpty());
+		assertFalse(test1.isEmpty());
 	}
 
 }
