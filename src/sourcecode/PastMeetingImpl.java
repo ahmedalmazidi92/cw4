cc3cbe10d6d2b9a5ab6ae10c69b9114a82774450
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-01-02 12:46:37 +0300

    Removed addNotes()

--- src/sourcecode/PastMeetingImpl.java
+++ src/sourcecode/PastMeetingImpl.java
@@ -8,18 +8,14 @@ import interfaces.PastMeeting;
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	String notes;
 
-	public PastMeetingImpl(Calendar date, Set<Contact> contacts) {
+	public PastMeetingImpl(Calendar date, Set<Contact> contacts, String notes) {
 		super(date, contacts);
-		this.notes = "";
+		this.notes = notes;
 		
 	}
 
 	public String getNotes() {
 		return notes;
-	}
-	
-	public void addNotes(String notes) {
-		this.notes = notes;
 	}
 
 }
