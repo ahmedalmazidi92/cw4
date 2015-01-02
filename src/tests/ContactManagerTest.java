d0aa6bf02ca7f2a913a2bdd9d4ac9e29cffd0fe8
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-01-02 12:25:47 +0300

    Removed the //. Will change the tests

--- src/tests/ContactManagerTest.java
+++ src/tests/ContactManagerTest.java
@@ -21,25 +21,11 @@ import interfaces.FutureMeeting;
 public class ContactManagerTest {
 	private ContactManager test1;
 	private Calendar date1;
-	private Set<Contact> contacts;
-	private Contact simon;
-	private Contact nick;
-	private Contact edgar;
-	private Contact scott;
 
 	@Before
 	public void buildUp() {
 		test1 = new ContactManagerImpl();
-		simon = new ContactImpl("Simon Pegg", "Recovering from Paul");
-		nick = new ContactImpl("Nick Frost", "Also recovering from Paul");
-		edgar = new ContactImpl("Edgar Wright", "Is glad to not have worked on Paul");
-		scott = new ContactImpl("Scott Pilgrim");
 		date1 = new GregorianCalendar(2015, 03, 05);
-		contacts = new HashSet<Contact>();
-		contacts.add(simon);
-		contacts.add(nick);
-		contacts.add(edgar);
-		contacts.add(scott);
 	}
 	
 	@After
@@ -48,23 +34,23 @@ public class ContactManagerTest {
 		ContactImpl.count = 0;
 	}
 	
-	//@Test
-	//public void testAddFutureMeeting() { //works
-		//int expected = 1;
-		//int actual = test1.addFutureMeeting(contacts, date1);
-		//assertEquals(expected, actual);
-	//}
+	@Test
+	public void testAddFutureMeeting() { //works
+		int expected = 1;
+		int actual = test1.addFutureMeeting(contacts, date1);
+		assertEquals(expected, actual);
+	}
 	
-	//@Test(expected = IllegalArgumentException.class) //works
-	//public void testInvalidDate() {
-		//date1 = new GregorianCalendar(2013, 1, 1);
-		//test1.addFutureMeeting(contacts, date1);
-	//}
+	@Test(expected = IllegalArgumentException.class) //works
+	public void testInvalidDate() {
+		date1 = new GregorianCalendar(2013, 1, 1);
+		test1.addFutureMeeting(contacts, date1);
+	}
 	
-	//@Test(expected = IllegalArgumentException.class)
-	//public void testInvalidContact() {
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidContact() {
 		
-	//}
+	}
 	
 	@Test
 	public void testAddContacts() { //works
