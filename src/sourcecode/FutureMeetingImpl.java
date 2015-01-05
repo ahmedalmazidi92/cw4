cf3ba61aed8e75c2b1a52f4f94135ad8e10b3544
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-01-05 12:15:51 +0000

    Overrode toString() and implemented serializable

--- src/sourcecode/FutureMeetingImpl.java
+++ src/sourcecode/FutureMeetingImpl.java
@@ -3,13 +3,19 @@ package sourcecode;
 import interfaces.Contact;
 import interfaces.FutureMeeting;
 
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Set;
 
-public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting, Serializable {
 
 	public FutureMeetingImpl(Calendar date, Set<Contact> contacts) {
 		super(date, contacts);
 	}
-
+	
+	@Override
+	public String toString() {
+		return ("Future Meeting [ID: " + getId() + ", Date: " + (getDate().get(Calendar.DATE)) + (getDate().get(Calendar.MONTH)) + (getDate().get(Calendar.YEAR)) 
+				+ ", Contacts: " + getContacts());
+	}
 }
