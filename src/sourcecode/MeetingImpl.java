1c49494bb13048703d3995206f91d1252ea161c2
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-01-05 17:18:26 +0000

    Added a generate serialVersiosUID. Hopefully it would help sort out the issues

--- src/sourcecode/MeetingImpl.java
+++ src/sourcecode/MeetingImpl.java
@@ -19,6 +19,10 @@ import interfaces.Meeting;
  * 
  */
 public class MeetingImpl implements Meeting, Comparator<Meeting>, Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2885050652332298626L;
 	private int id;
 	private Calendar date;
 	private Set<Contact> contacts;
