725e8c158ff3f3b28ee4602fa0fa1f85eb3ef655
Ahmed Almazidi <a.almazidi92@gmail.com>
2015-01-05 12:14:48 +0000

    Implemented serializable and overrode toString()

--- src/sourcecode/PastMeetingImpl.java
+++ src/sourcecode/PastMeetingImpl.java
@@ -1,22 +1,18 @@
 package sourcecode;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Set;
 
 import interfaces.Contact;
 import interfaces.PastMeeting;
 
-public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting, Serializable {
 	String notes;
 
 	public PastMeetingImpl(Calendar date, Set<Contact> contacts, String notes) {
 		super(date, contacts);
 		this.notes = notes;
 		
-	}
-	
-	public PastMeetingImpl(PastMeeting oldMeeting, String notes){
-		super(oldMeeting.getDate(), oldMeeting.getContacts());
-		this.notes = notes;
 	}
 
 	public String getNotes() {
@@ -25,5 +21,11 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	
 	public void setNotes(String notes) {
 		this.notes = notes;
+	}
+	
+	@Override
+	public String toString() {
+		return ("Past Meeting [ID: " + getId() + ", Date: " + (getDate().get(Calendar.DATE)) + (getDate().get(Calendar.MONTH)) + (getDate().get(Calendar.YEAR)) 
+				+ ", Contacts: " + getContacts());
 	}
 }
